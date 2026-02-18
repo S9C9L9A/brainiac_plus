a5312e49107cc1006e0f618ef988cf7cfd1760c1
Genna G <gennag999@gmail.com>
2026-02-18 17:48:02 +0100

    feat: add Linux platform support to resource controller

--- lib/features/dashboard/controllers/resource_controller.dart
+++ lib/features/dashboard/controllers/resource_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:brainiac_plus/core/platform/linux_platform.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
